fb00ec974b96ca4fc01326b53043177c7a72baa1
ercross <tobins4u@gmail.com>
2024-11-08 15:50:38 +0100

    added light theme

---
+++ lib/app/ui/theme.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:kudu/app/ui/colors.dart';
+
+abstract class AppTheme {
+  static const String _font = "Proxima";
+  static final ThemeData light = ThemeData(
+      fontFamily: _font,
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ButtonStyle(
+          minimumSize: WidgetStateProperty.resolveWith<Size>(
+              (_) => const Size(double.infinity, 46)),
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          backgroundColor:
+              WidgetStateProperty.resolveWith<Color>((_) => AppColor.primary),
+          shape: WidgetStateProperty.resolveWith<OutlinedBorder>((_) =>
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10))),
+          textStyle:
+              WidgetStateProperty.resolveWith<TextStyle>((_) => TextStyle(
+                    fontSize: 14,
+                    foreground: Paint()..color = Colors.white,
+                    fontFamily: _font,
+                    fontWeight: FontWeight.w500,
+                  )),
+        ),
+      ));
+}
